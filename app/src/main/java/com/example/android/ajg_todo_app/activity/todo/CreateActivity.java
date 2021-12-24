f91c64ab1fb6db2a525d101810db20b48a1d1987
Andreas Jonathan <andreas.jonathan@binus.edu>
2021-12-24 17:21:00 +0700

    feat: Init View (HomeActivity, UpdateActivity)

--- app/src/main/java/com/example/android/ajg_todo_app/activity/todo/CreateActivity.java
+++ app/src/main/java/com/example/android/ajg_todo_app/activity/todo/CreateActivity.java
@@ -2,15 +2,49 @@ package com.example.android.ajg_todo_app.activity.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.android.ajg_todo_app.R;
 
 public class CreateActivity extends AppCompatActivity {
 
+    Intent intent;
+    EditText etName, etDescription;
+    Button btnSave;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        this.initView();
+    }
+
+    private void initView() {
+        etName = (EditText) findViewById(R.id.create_et_name);
+        etDescription = (EditText) findViewById(R.id.create_et_description);
+        btnSave = (Button) findViewById(R.id.create_btn_save);
+        this.onClickBtnSave();
+    }
+
+    private void onClickBtnSave() {
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // validasi data
+
+
+                // masukkin ke db
+
+
+                // kalo berhasil create, pindah activity
+                intent = new Intent(CreateActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/ajg_todo_app/activity/todo/UpdateActivity.java
+++ app/src/main/java/com/example/android/ajg_todo_app/activity/todo/UpdateActivity.java
@@ -2,15 +2,48 @@ package com.example.android.ajg_todo_app.activity.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.android.ajg_todo_app.R;
 
 public class UpdateActivity extends AppCompatActivity {
 
+    Intent intent;
+    EditText etName, etDescription;
+    Button btnSave;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+    }
+
+    private void initView() {
+        etName = (EditText) findViewById(R.id.update_et_name);
+        etDescription = (EditText) findViewById(R.id.update_et_description);
+        btnSave = (Button) findViewById(R.id.update_btn_save);
+        this.onClickBtnSave();
+    }
+
+    private void onClickBtnSave() {
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // validasi data
+
+
+                // update ke db
+
+
+                // kalo update berhasil, pindah activity
+                intent = new Intent(UpdateActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
